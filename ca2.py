086d4211a85db7cb0ea8c4b4346e7d7d8eb09405
Ahmadreza Saboor <ahmadreza.saboor2012@gmail.com>
2018-04-15 17:09:31 +0430

    add global vars

---
+++ ca2.py
@@ -0,0 +1,35 @@
+
+d, t = 0
+course_cnt = 0
+course_happiness = []
+prof_cnt = 0
+prof_course = {}
+conflict_table = []
+
+def init():
+	pass
+
+def read_input():
+	global d, t, c
+	d, t = map(int, raw_input().split(' '))
+	course_cnt = input()
+	course_happiness = map(int, raw_input().)
+
+
+def eval():
+	pass
+
+def selection():
+	pass
+
+def mutation():
+	pass
+
+def crossover():
+	pass
+
+def main():
+	read_input()
+
+
+main()
